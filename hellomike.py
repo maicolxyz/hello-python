2c7f12d92235bfe75845cda99661984133166a84
Michael Maurial <91722863+maicolxyz@users.noreply.github.com>
2022-07-11 00:32:59 -0400

    Added file via upload

---
+++ hellomike.py
@@ -0,0 +1,20 @@
+# introduction
+
+print(2 + 2) # addition
+print(4 - 2) # subtraction
+print(2 * 2) # multiplication
+print(4 / 2) # division
+print(4 ** 2) # exponentiation 
+print(6 % 5) # modulus 
+print(6 // 5) # floor division operator 
+
+# check data types 
+
+print(type(10)) # integer (int)
+print(type(2.22)) # float
+print(type(1 + 4j)) # complex
+print(type('Mike')) # string
+print(type([1, 2 , 3])) # list
+print(type({'name':'Mike'})) # dictionary
+print(type({12.8, 6.4, 3.2})) # set
+print(type((12.8, 6.4, 3.2))) # tuple
